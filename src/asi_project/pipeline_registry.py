fd9bbc3e574d76ae075257561cb994d3690ec296
Jan Jaroszczyk <141874028+faust2077@users.noreply.github.com>
2025-06-20 17:26:32 +0200

    Update pipeline configuration

--- src/asi_project/pipeline_registry.py
+++ src/asi_project/pipeline_registry.py
@@ -1,16 +1,16 @@
 """Project pipelines."""
 
 from kedro.pipeline import Pipeline
-from your_project_name.pipelines import preprocessing, model_training
+from asi_project.pipelines import data_processing, model_training
 
 def register_pipelines() -> dict[str, Pipeline]:
     """Rejestracja wszystkich pipeline'ów"""
     
-    preprocessing_pipeline = preprocessing.create_pipeline()
+    data_processing_pipeline = data_processing.create_pipeline()
     model_training_pipeline = model_training.create_pipeline()
     
     return {
-        "preprocessing": preprocessing_pipeline,
+        "data_processing": data_processing_pipeline,
         "model_training": model_training_pipeline,
-        "__default__": preprocessing_pipeline + model_training_pipeline,
+        "__default__": data_processing_pipeline + model_training_pipeline,
     }
